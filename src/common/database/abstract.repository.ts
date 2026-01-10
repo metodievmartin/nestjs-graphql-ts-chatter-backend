dfb71ed76281564ed3d12c70bcdebdb98530c8cb
Martin Metodiev <martin.metodiew@gmail.com>
2026-01-10 22:58:33 +0200

    Create abstract Repository and abstract Entity classes to work with mongoose

---
+++ src/common/database/abstract.repository.ts
@@ -0,0 +1,64 @@
+import { Logger, NotFoundException } from '@nestjs/common';
+import type { Filter, UpdateFilter } from 'mongodb';
+import { Model, Types } from 'mongoose';
+
+import { AbstractEntity } from './abstract.entity';
+
+export abstract class AbstractRepository<T extends AbstractEntity> {
+  protected abstract readonly logger: Logger;
+
+  constructor(protected readonly model: Model<T>) {}
+
+  async create(document: Omit<T, '_id'>): Promise<T> {
+    const createdDocument = new this.model({
+      ...document,
+      _id: new Types.ObjectId(),
+    });
+
+    return (await createdDocument.save()).toJSON() as unknown as T;
+  }
+
+  async findOne(filterQuery: Filter<T>): Promise<T> {
+    const document = await this.model.findOne(filterQuery, {}).lean<T>();
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document;
+  }
+
+  async findOneAndUpdate(
+    filterQuery: Filter<T>,
+    update: UpdateFilter<T>,
+  ): Promise<T> {
+    const document = await this.model
+      .findOneAndUpdate(filterQuery, update, {
+        new: true,
+      })
+      .lean<T>();
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document;
+  }
+
+  async find(filterQuery: Filter<T>): Promise<T[]> {
+    return this.model.find(filterQuery).lean<T[]>();
+  }
+
+  async findOneAndDelete(filterQuery: Filter<T>): Promise<T> {
+    const document = await this.model.findOneAndDelete(filterQuery).lean<T>();
+
+    if (!document) {
+      this.logger.warn('Document was not found with filterQuery', filterQuery);
+      throw new NotFoundException('Document not found.');
+    }
+
+    return document;
+  }
+}
