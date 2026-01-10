d4d4bbb3af44092ef62db1c423d4defafd1e1c71
Martin Metodiev <martin.metodiew@gmail.com>
2026-01-10 15:35:40 +0200

    Set up the ConfigService and MongoDB database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
+import Joi from 'joi';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { DatabaseModule } from './common/database/database.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+      }),
+    }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
